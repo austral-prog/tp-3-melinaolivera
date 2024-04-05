2244ca0c0278cf8be7f7762b7b6430f91e5c6924
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-04-05 20:39:41 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,19 @@
 def check_vowels():
     # Código a implementar utilizando input.
+    input(Matias)
+    print(f"contiene a: {"a" in "Matias"}")
+    print(f"contiene e: {"e" in "Matias"}")
+    print(f"contiene i: {"i" in "Matias"}")
+    print(f"contiene o: {"o" in "Matias"}")
+    print(f"contiene u: {"u" in "Matias"}")
 
+
+    input(Augusto)
+    print(f" contiene a: {"a" in "Augusto"}")
+    print(f" contiene e: {"e" in "Augusto"}")
+    print(f" contiene i: {"i" in "Augusto"}")
+    print(f" contiene o: {"o" in "Augusto"}")
+    print(f" contiene u: {"u" in "Augusto"}")
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
