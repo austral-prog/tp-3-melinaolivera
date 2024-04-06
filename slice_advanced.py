65b01395ea8f92604bfe9944c6ee725247f85a7c
melinaolivera <162723084+melinaolivera@users.noreply.github.com>
2024-04-05 21:15:22 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,9 +1,7 @@
 def slice_advanced():
-    texto = "Awesome"
-    # Código a implementar, se debe utilizar la variable 'texto' para resolver el ejercicio.
-    # No se debe modificar la definicion de la funcion, ni ingresar otro valor mediante input. 
-    print (texto.lower() [0:3])
-    print (texto [2:5])
-    print (texto.lower() [0:7])
+    # Código a implementar utilizando input.
+    frase= input(">")
+    print (frase [4::2])
+    
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
